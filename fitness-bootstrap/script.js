cca32c23ef16f6fd46e020154dc8feff2a8641da
roggenbailey <baileyroggen@gmail.com>
2025-01-07 19:02:22 -0800

    Add files via upload

---
+++ fitness-bootstrap/script.js
@@ -0,0 +1,10 @@
+/*
+    Student Name: Bailey
+    File Name: script.js
+    Date: 5/20
+*/
+
+//jQuery for hero image to consume the header window space
+$(document).ready(function(){
+	$('.hero').height($(window).height());
+});
